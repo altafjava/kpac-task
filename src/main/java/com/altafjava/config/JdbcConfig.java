116ffb5e45672e130cb27d11764f2f6689237173
Altaf <samaltaf6@gmail.com>
2022-04-21 12:28:49 +0530

    add README, sql-script & database.properties files

--- src/main/java/com/altafjava/config/JdbcConfig.java
+++ src/main/java/com/altafjava/config/JdbcConfig.java
@@ -1,29 +1,37 @@
 package com.altafjava.config;
 
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import com.altafjava.constant.AppConstant;
 
 @Configuration
 @ComponentScan("com.altafjava.repository")
+@PropertySource(value = { "classpath:database.properties" })
 public class JdbcConfig {
+
+	@Autowired
+	private Environment environment;
 
 	@Bean
 	public DataSource dataSource() {
-		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/kpac");
-		dataSource.setUsername("root");
-		dataSource.setPassword("root");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty(AppConstant.MYSQL_DRIVER_CLASS_NAME));
+		dataSource.setUrl(environment.getProperty(AppConstant.MYSQL_URL));
+		dataSource.setUsername(environment.getProperty(AppConstant.MYSQL_USERNAME));
+		dataSource.setPassword(environment.getProperty(AppConstant.MYSQL_PASSWORD));
 		return dataSource;
 	}
 
 	@Bean
 	public JdbcTemplate jdbcTemplate() {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource());
-		return jdbcTemplate;
+		System.out.println("jdbctemplate=" + dataSource());
+		return new JdbcTemplate(dataSource());
 	}
 }
---
+++ src/main/java/com/altafjava/constant/AppConstant.java
@@ -0,0 +1,8 @@
+package com.altafjava.constant;
+
+public class AppConstant {
+	public static final String MYSQL_DRIVER_CLASS_NAME = "MYSQL_DRIVER_CLASS_NAME";
+	public static final String MYSQL_URL = "MYSQL_URL";
+	public static final String MYSQL_USERNAME = "MYSQL_USERNAME";
+	public static final String MYSQL_PASSWORD = "MYSQL_PASSWORD";
+}
